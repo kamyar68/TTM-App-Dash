8412ebf886685292b81a2a6055303df578e006af
kamyar68 <kamyar.hasanzadeh@gmail.com>
2024-11-22 14:04:56 +0200

    home page empty layout added

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ app.layout = dbc.Container([
     html.Div(id='page-content'),
 ])
 
+
 # Callback to dynamically serve pages based on URL
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
@@ -22,12 +23,79 @@ def display_page(pathname):
     elif pathname == '/AB_map':
         return toast_map_layout
     else:
+        # Return the custom home page layout
         return html.Div([
-            html.H2("Welcome to the Dashboard"),
-            dcc.Link("Go to Matrix Map", href='/matrix'),
-            html.Br(),
-            dcc.Link("Go to A-B Map", href='/AB_map'),
+            # Heading
+            html.Div(
+                html.H1("Welcome to the Dashboard", style={"textAlign": "center", "marginBottom": "20px"}),
+                style={"padding": "20px"}
+            ),
+
+            # Row with images and text/links
+            dbc.Row(
+                [
+                    dbc.Col(
+                        html.Div(
+                            [
+                                html.Img(src="/assets/image1.jpg", style={"width": "100%"}),
+                                html.A("Go to Matrix Map", href="/matrix",
+                                       style={"display": "block", "textAlign": "center", "marginTop": "10px"})
+                            ]
+                        ),
+                        width=4
+                    ),
+                    dbc.Col(
+                        html.Div(
+                            [
+                                html.Img(src="/assets/image2.jpg", style={"width": "100%"}),
+                                html.A("Go to A-B Map", href="/AB_map",
+                                       style={"display": "block", "textAlign": "center", "marginTop": "10px"})
+                            ]
+                        ),
+                        width=4
+                    ),
+                    dbc.Col(
+                        html.Div(
+                            [
+                                html.Img(src="/assets/image3.jpg", style={"width": "100%"}),
+                                html.A("More Info", href="#",
+                                       style={"display": "block", "textAlign": "center", "marginTop": "10px"})
+                            ]
+                        ),
+                        width=4
+                    ),
+                ],
+                style={"marginBottom": "40px"}
+            ),
+
+            # Static text box
+            html.Div(
+                html.P(
+                    """
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    """,
+                    style={
+                        "padding": "20px",
+                        "border": "1px solid #ddd",
+                        "borderRadius": "5px",
+                        "backgroundColor": "#f9f9f9",
+                        "fontSize": "16px",
+                        "lineHeight": "1.5",
+                        "marginBottom": "20px"
+                    }
+                )
+            ),
+
+            # Bottom image
+            html.Div(
+                html.Img(src="/assets/image4.jpg", style={"width": "20%"}),
+                style={"textAlign": "center"}
+            )
         ])
+
 
 # Run the app
 if __name__ == '__main__':
